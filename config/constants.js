04a0bd93682696d4a820a43fd395a66c35803118
Vittor Patricio <121522773+VittorPatricio@users.noreply.github.com>
2025-03-10 22:11:38 -0300

    Alteração chave api gemini

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,5 @@
 export default {
   GEMINI_API_URL:
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent",
-  API_GEMINI_KEY: "AIzaSyCbzhG4VwVhNKlPAvltpShRpJscjXwEBI4",
+  API_GEMINI_KEY: "inserir_chave_api_gemini_aqui",
 };
